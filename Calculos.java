bed8b400be36d1ef1da9766788b6bfcd7c9cfe78
MIguelAvilaLuperiPerezGomez <migueltkd63@gmail.com>
2026-04-07 07:51:05 -0300

    Add files via upload

---
+++ Calculos.java
@@ -0,0 +1,19 @@
+public class Calculos {
+    public  void Soma(double a, double b){
+        System.out.println(a + b);
+    }
+    public void Sub(double a, double b){
+        System.out.println(a - b);
+    }
+    public void Mut(double a, double b){
+        System.out.println(a * b);
+    }
+    public void Div(double a, double b){
+        System.out.println(a / b);
+    }
+
+
+    public Calculos(){
+
+    }
+}
